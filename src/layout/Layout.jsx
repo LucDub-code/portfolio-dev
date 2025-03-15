7fb8c0a4df96db96505fa2ca4adbdf468bd5c6c6
LucDub-code <lucdubcom@gmail.com>
2025-03-15 20:02:35 +0100

    Importation des composants layout dans Layout et de Layout dans App

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import SideMenu from "./SideMenu"
+import EditorContent from "./EditorContent"
+
+export default function Layout() {
+  return (
+    <div>Layout</div>
+  )
+}
