1d2c1ab15d926b2a8ffcdca02b03f42c8b7d170b
LucDub-code <contact@lucasdubeau.dev>
2025-03-15 20:02:35 +0100

    Importation des composants layout dans Layout et de Layout dans App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Layout from "./layout/Layout"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Layout />
     </>
   )
 }
