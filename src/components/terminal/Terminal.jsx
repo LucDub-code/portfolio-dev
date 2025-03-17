e63547f370e8af3f6b03e7b49416a797440e5104
LucDub-code <contact@lucasdubeau.dev>
2025-03-17 16:30:04 +0100

    Crátion de la StatusBar

--- src/components/terminal/Terminal.jsx
+++ src/components/terminal/Terminal.jsx
@@ -4,7 +4,7 @@ import DebugControls from './DebugControls'
 
 export default function Terminal() {
   return (
-    <div className="h-full flex flex-col bg-bg-terminal border-t border-border-ide">
+    <div className="flex-1 flex flex-col bg-bg-terminal border-t border-border-ide">
       <TerminalHeader />
       <TerminalContent />
       <DebugControls />
--- src/components/terminal/TerminalContent.jsx
+++ src/components/terminal/TerminalContent.jsx
@@ -1,6 +1,6 @@
 export default function TerminalContent() {
   return (
-    <div className="h-full p-4 flex justify-between text-text-default">
+    <div className="flex-1 p-4 flex justify-between text-text-default">
       <div>time: 30s</div>
       <div>score: 0</div>
     </div>
