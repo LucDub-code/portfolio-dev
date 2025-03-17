c7258bd09e1520bd9701c220221c5ef805590596
LucDub-code <contact@lucasdubeau.dev>
2025-03-17 16:01:27 +0100

    Crátion du Terminal et de ses composants

--- src/components/terminal/Terminal.jsx
+++ src/components/terminal/Terminal.jsx
@@ -1,7 +1,13 @@
+import TerminalHeader from './TerminalHeader'
+import TerminalContent from './TerminalContent'
+import DebugControls from './DebugControls'
+
 export default function Terminal() {
   return (
-    <div>
-      
+    <div className="h-full flex flex-col bg-bg-terminal border-t border-border-ide">
+      <TerminalHeader />
+      <TerminalContent />
+      <DebugControls />
     </div>
   )
 }
--- src/components/terminal/TerminalContent.jsx
+++ src/components/terminal/TerminalContent.jsx
@@ -1,7 +1,8 @@
 export default function TerminalContent() {
   return (
-    <div>
-      
+    <div className="h-full p-4 flex justify-between text-text-default">
+      <div>time: 30s</div>
+      <div>score: 0</div>
     </div>
   )
 }
