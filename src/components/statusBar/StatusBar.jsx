6625e5a073769919e0e09efc30acfb735c44d184
LucDub-code <contact@lucasdubeau.dev>
2025-03-17 16:39:02 +0100

    Message StatusBar en fonction de la navigation

--- src/components/statusBar/StatusBar.jsx
+++ src/components/statusBar/StatusBar.jsx
@@ -1,7 +1,18 @@
+import { useNavigation } from '../context/NavigationContext'
+
 export default function StatusBar() {
+  const { activePage } = useNavigation()
+
+  const statusMessages = {
+    'hello-world': '// Cliquez sur les bugs pour les corriger',
+    'a-propos': '// Utilisez les onglets pour naviguer',
+    'projets': '// Revenez bientôt pour découvrir mes nouveaux projets',
+    'contact': '// Utilisez ce formulaire pour me contacter'
+  }
+
   return (
     <div className="h-6 bg-statusBar-background border-t border-border-ide text-text-default text-sm px-4 flex items-center justify-center">
-      // Cliquez sur les bugs pour les corriger
+      {statusMessages[activePage]}
     </div>
   )
 }
