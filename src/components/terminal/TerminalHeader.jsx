34476019d90dff2efd658ac085f6b5b5f70bc7d7
LucDub-code <lucdubcom@gmail.com>
2025-03-17 16:01:27 +0100

    Crátion du Terminal et de ses composants

--- src/components/terminal/TerminalHeader.jsx
+++ src/components/terminal/TerminalHeader.jsx
@@ -1,7 +1,7 @@
 export default function TerminalHeader() {
   return (
-    <div>
-      
+    <div className="px-4 py-1 text-sm text-center text-text-default border-b border-border-ide">
+      <p>debug.js</p>
     </div>
   )
 }
