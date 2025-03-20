42e0b1815b9ed0a81098ddef64e9bc18be2d25b3
LucDub-code <lucdubcom@gmail.com>
2025-03-20 20:59:17 +0100

    Effet rotating conic gradient sur le bouton START du jeux

--- src/components/terminal/Terminal.jsx
+++ src/components/terminal/Terminal.jsx
@@ -1,13 +1,13 @@
 import TerminalHeader from './TerminalHeader'
-import TerminalContent from './TerminalContent'
 import BugSquashGame from '../game/BugSquashGame'
 
 export default function Terminal({ className }) {
   return (
     <div className={`flex flex-col bg-bg-terminal border-t border-border-ide ${className || ''}`}>
       <TerminalHeader />
-      <TerminalContent />
-      <BugSquashGame />
+      <div className="flex-1 flex justify-center items-center">
+        <BugSquashGame />
+      </div>
     </div>
   )
 }
