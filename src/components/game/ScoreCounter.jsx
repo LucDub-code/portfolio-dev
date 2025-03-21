a9a307397a3588e2539661bdd87fa32a7311c8ec
LucDub-code <lucdubcom@gmail.com>
2025-03-21 02:19:35 +0100

    V1 du jeux avec problème de visibilité de l'animation check lottie

--- src/components/game/ScoreCounter.jsx
+++ src/components/game/ScoreCounter.jsx
@@ -1,7 +1,12 @@
-export default function ScoreCounter() {
+import { useEffect } from 'react';
+
+export default function ScoreCounter({ score = 0, gameActive = false }) {
+  // Le score est passé en prop depuis le composant parent
+  
   return (
-    <div>
-      
+    <div className="flex items-center">
+      <span className="text-green-number font-mono font-bold text-base mr-1">Score:</span>
+      <span className="text-text-default font-mono text-base">{score}</span>
     </div>
-  )
+  );
 }
