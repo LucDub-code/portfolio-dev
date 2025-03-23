d7d82898a4ae39c308533596b583876e4840968f
LucDub-code <contact@lucasdubeau.dev>
2025-03-23 01:56:45 +0100

    Travail de mise en forme en fonction du résultat de l'hébergement

--- src/components/game/ScoreCounter.jsx
+++ src/components/game/ScoreCounter.jsx
@@ -18,18 +18,18 @@ export default function ScoreCounter({ score = 0, gameActive = false }) {
   // Déterminer les classes en fonction de la taille de l'écran
   const getTextSizeClasses = () => {
     if (windowWidth <= 321) {
-      return 'text-xs';
+      return 'text-[0.6rem]';
     } else if (windowWidth <= 376) {
-      return 'text-sm';
+      return 'text-[0.75rem]';
     } else if (windowWidth <= 426) {
-      return 'text-sm';
+      return 'text-[0.8rem]';
     } else {
-      return 'text-base';
+      return 'text-sm sm:text-base';
     }
   };
   
   return (
-    <div className="flex items-center">
+    <div className="flex items-center mt-0.5">
       <span className={`text-green-number font-mono font-bold ${getTextSizeClasses()} mr-1`}>Score:</span>
       <span className={`text-text-default font-mono ${getTextSizeClasses()}`}>{score}</span>
     </div>
