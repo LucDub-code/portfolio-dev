05ae6a4970c4397ca071235c21a60a5c62375424
LucDub-code <lucdubcom@gmail.com>
2025-08-28 16:20:53 +0200

    Création des routes et routers de l'api

--- backend-portfolio/app.js
+++ backend-portfolio/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const authRoutes = require('./routes/auth');
+const projectsRoutes = require('./routes/projects');
+
+const app = express();
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.set("port", process.env.PORT || 3000);
 
@@ -10,5 +24,8 @@ app.use(cors({
   origin: ['http://localhost:5173', 'https://lucasdubeau.dev'],
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectsRoutes);
 
 module.exports = app;
