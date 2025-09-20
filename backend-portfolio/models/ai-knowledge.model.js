970826daef8c11f999070160eba82d23e7f43d9a
LucDub-code <contact@lucasdubeau.dev>
2025-09-20 04:11:48 +0200

    Création du controller chat avec pour la génération des embeddings des prompts utilisateurs et recherche vectorielle dans la base de connaissance. Optimisation RAG, threshold à 0.895 optimal, ajout de keywords et de questions dans la construction des embeddings utilisateurs et refacto de certains contenus pour éviter que les prompts borderlines ne passent.

--- backend-portfolio/models/ai-knowledge.model.js
+++ backend-portfolio/models/ai-knowledge.model.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const aiKnowledgeSchema = new mongoose.Schema({
   content: { type: String, required: true },
+  questions: { type: [String], required: true },
+  keywords: { type: [String], required: true },
   source: { type: String, required: true },
   embedding: { type: [Number], required: true },
   content_hash: { type: String, required: true }
