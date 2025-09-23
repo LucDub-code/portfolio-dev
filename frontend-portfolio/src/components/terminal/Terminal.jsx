2e6325a6b64f2fbd3ae8467a18966b71ebc14381
LucDub-code <lucdubcom@gmail.com>
2025-09-23 18:49:37 +0200

    Test fix: padding bottom nécéssaire sur Terminal en version mobile

--- frontend-portfolio/src/components/terminal/Terminal.jsx
+++ frontend-portfolio/src/components/terminal/Terminal.jsx
@@ -5,7 +5,7 @@ export default function Terminal() {
   return (
     <div className="flex flex-col bg-bg-terminal border-t border-border-ide relative h-[calc(100%-410px)] max-[425px]:h-[calc(100%-290px)] max-[320px]:h-[calc(100%-260px)]">
       <TerminalHeader />
-      <div className='pb-8 pt-14 max-[770px]:pb-12 max-[425px]:pb-5.5 max-[425px]:pt-1.5 px-8 max-[425px]:px-2 flex-1 h-full'>
+      <div className='pb-8 pt-14 max-[770px]:pb-12 max-[425px]:pb-24 max-[425px]:pt-1.5 px-8 max-[425px]:px-2 flex-1 h-full'>
         <AiChat />
       </div>
     </div>
